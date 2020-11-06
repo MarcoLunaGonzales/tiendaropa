ca4ee77cc22e36673913520bdc51b3a046ddb9bc
marquexmar <lunagonzalesmarco@gmail.com>
2020-11-06 16:27:40 -0400

    cambios minimos

--- grupos/configModule.php
+++ grupos/configModule.php
@@ -3,6 +3,13 @@ $table="grupos";
 $moduleNameSingular="Grupo";
 $moduleNamePlural="Grupos";
 
+
+$tableDetalle="subgrupos";
+$moduleDetNameSingular="Sub-Grupo";
+$moduleDetNamePlural="Sub-Grupos";
+
+$campoForaneo="cod_grupo";
+
 $urlList="grupos/list.php";
 $urlList2="list.php";
 $urlRegister="register.php";
@@ -10,6 +17,15 @@ $urlEdit="edit.php";
 $urlDelete="delete.php";
 $urlSave="save.php";
 $urlSaveEdit="saveEdit.php";
+$urlSaveDelete="";
+
+$urlListDetalle="grupos/listDetalle.php";
+$urlListDetalle2="listDetalle.php";
+$urlRegisterDet="registerDetalle.php";
+$urlEditDet="editDetalle.php";
+$urlDeleteDet="deleteDetalle.php";
+$urlSaveDet="saveDetalle.php";
+$urlSaveEditDet="saveEditDetalle.php";
 $urlSaveDelete="";
 
 ?>
