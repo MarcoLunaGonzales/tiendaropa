fde0f9643a7e21b2f24cde90754b7a11a21f1768
gabriela quelali <gabrielaquelali@gmail.com>
2023-08-22 14:45:22 -0400

    cambios ventas

--- navegadorVentas.php
+++ navegadorVentas.php
@@ -579,6 +579,7 @@ while ($dat = mysqli_fetch_array($resp)) {
     if($codTipoDoc==1){
         echo "<td  bgcolor='$color_fondo'>
 		<a href='formatoFactura.php?codVenta=$codigo' target='_BLANK'><img src='imagenes/factura1.jpg' width='30' border='0' title='Factura Formato Pequeño'></a>
+        <a href='formatoFacturaEspania.php?codVenta=$codigo' target='_BLANK'>factura</a>
 		
 		</td>";
     }else{
