dc6488cce6a248208759dd2cdf8b33c8ae828dc1
gabriela quelali <gabrielaquelali@gmail.com>
2023-08-28 18:24:41 -0400

    Actualizacion Precios

--- listaPrecios.php
+++ listaPrecios.php
@@ -17,39 +17,66 @@ echo "<center><table class='texto'>";
 	echo "<tr>
 	<th>Nro</th><th>Sucursal</th><th>&nbsp;</th><th>Precio</th><th>Cantidad Inicio</th><th>Cantidad Final</th>		
 		</tr>";
-		$sqlListPrecios="select p.codigo_material,p.cod_precio,gp.nombre ,gp.abreviatura ,p.precio,p.cod_ciudad,c.nombre_ciudad,c.descripcion,
-		p.cant_inicio,p.cant_final, p.created_by, 
-		concat(f.nombres,' ',f.paterno,' ',f.materno) as creado_por, p.created_date,
-		p.modified_by, 
-		concat(f1.nombres,' ',f1.paterno,' ',f1.materno) as modificado_por,
-		p.modified_date
-		from precios p
-		left join grupos_precio gp on (p.cod_precio=gp.codigo)
-		left join ciudades c on (p.cod_ciudad=c.cod_ciudad)
-		left join funcionarios f on (p.created_by=f.codigo_funcionario)
-		left join funcionarios f1 on (p.modified_by=f1.codigo_funcionario)
-		where p.codigo_material='".$codigo."' order by c.nombre_ciudad asc,p.cod_precio asc";
-		$respListPrecios=mysqli_query($enlaceCon,$sqlListPrecios);
-		$indice_tabla=1;
-		while($datListPrecios=mysqli_fetch_array($respListPrecios)){
 
-			$cod_precio=$datListPrecios['cod_precio'];
-			$nombre=$datListPrecios['nombre'];
-			$abreviatura=$datListPrecios['abreviatura'];
-			$precio=$datListPrecios['precio'];
-			$cod_ciudad=$datListPrecios['cod_ciudad'];
-			$nombre_ciudad=$datListPrecios['nombre_ciudad'];
-			$descripcion_ciudad=$datListPrecios['descripcion'];
-			$cant_inicio=$datListPrecios['cant_inicio'];
-			$cant_final=$datListPrecios['cant_final'];
+		$sqlCiu="select cod_ciudad,descripcion as desc_ciudad,nombre_ciudad 
+		from ciudades order by cod_ciudad asc";
 
+		$respCiu=mysqli_query($enlaceCon,$sqlCiu);
+		
+		while($datCiu=mysqli_fetch_array($respCiu)){
 
-			echo "<tr><td>".$indice_tabla."</td><td>".$descripcion_ciudad."</td><td>".$nombre."</td><td>".redondear2($precio)."</td>";
-			echo " <td>".$cant_inicio."</td><td>".$cant_final."</td></tr>";
-		$indice_tabla++;
+			$cod_ciudad=$datCiu['cod_ciudad'];
+			$desc_ciudad=$datCiu['desc_ciudad'];
+			$nombre_ciudad=$datCiu['nombre_ciudad'];		
 
+		$sqlGrupoPrecio="select codigo,nombre from grupos_precio where codigo<>0  and estado=1 order by codigo asc";
+
+		$respGrupoPrecio=mysqli_query($enlaceCon,$sqlGrupoPrecio);
+		
+			while($datGrupoPrecio=mysqli_fetch_array($respGrupoPrecio)){
+
+				$codGrupoPrecio=$datGrupoPrecio['codigo'];
+				$nomGrupoPrecio=$datGrupoPrecio['nombre'];			
+
+			$sqlPrecio="select p.codigo_material,p.precio, p.cant_inicio,p.cant_final, 
+			p.created_by, concat(f.nombres,' ',f.paterno,' ',f.materno) as creado_por, p.created_date,
+			p.modified_by, concat(f1.nombres,' ',f1.paterno,' ',f1.materno) as modificado_por,
+			p.modified_date from precios p		
+			left join funcionarios f on (p.created_by=f.codigo_funcionario)
+			left join funcionarios f1 on (p.modified_by=f1.codigo_funcionario)
+			where p.codigo_material='".$codigo."' and p.cod_ciudad='".$cod_ciudad."' and p.cod_precio='".$codGrupoPrecio."'";
+
+			$respPrecio=mysqli_query($enlaceCon,$sqlPrecio);
+			$cantSqlPrecio=mysqli_num_rows($respPrecio);
+
+	 		if($cantSqlPrecio>0){
+				while($datPrecio=mysqli_fetch_array($respPrecio)){
+					$precio=$datPrecio['precio'];
+					$cant_inicio=$datPrecio['cant_inicio'];
+					$cant_final=$datPrecio['cant_final'];
+					echo "<tr>";
+					echo "<td>&nbsp;</td>";
+					echo "<td>".$desc_ciudad."</td>";
+					echo "<td>".$nomGrupoPrecio."</td>";
+					echo "<td><input type='number' class='inputnumber'  id='precio' name='precio' size='6' min='0.1' step='0.01'  value='".redondear2($precio)."'></td>";
+					echo "<td><input class='inputnumber' type='number' min='1' id='cant_ini'  name='cant_ini' step='1' value='".$cant_inicio."' required></td>";
+					echo " <td><input class='inputnumber' type='number' min='1' id='cant_fin'  name='cant_fin' step='1' value='".$cant_final."' required></td>";
+					echo "</tr>";
+				}
+			}else{
+					echo "<tr>";
+					echo "<td>&nbsp;</td>";
+					echo "<td>".$desc_ciudad."</td>";
+					echo "<td>".$nomGrupoPrecio."</td>";
+					echo "<td><input type='number' class='inputnumber'  id='precio' name='precio' size='6' min='0.1' step='0.01'  value='0'></td>";
+					echo " <td><input class='inputnumber' type='number' min='1' id='cant_ini'  name='cant_ini' step='1' value='0' required></td>";
+					echo " <td><input class='inputnumber' type='number' min='1' id='cant_fin'  name='cant_fin' step='1' value='0' required></td>";
+					echo "</tr>";
+
+			}	
 		}
-
+	}			
+	
 echo"</table></center>";
 
 
@@ -58,4 +85,5 @@ echo "<div class='divBotones'>
 <input type='button' class='boton2' value='Cancelar' onClick='location.href=\"navegador_material.php\"'>
 </div>";
 echo "</form>";
+
 ?>
